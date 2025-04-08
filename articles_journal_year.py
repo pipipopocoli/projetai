32026876ed54a170280bbf0c52e3994e9d49736b
sauves <leonardsauve@gmail.com>
2025-04-08 18:11:21 -0400

    updated script to retrieve by volume, date, page

--- articles_journal_year.py
+++ articles_journal_year.py
@@ -3,51 +3,74 @@ import pdb
 import http
 import pandas as pd
 import re
+import numpy as np 
 
-yr_min = 2023
-yr_max = 2025
 base_url = "https://www.nature.com"
-
-search_url = f"{base_url}/search?order=relevance&date_range={yr_min}-{yr_max}&journal=ncomms"
-urls, auth, email, title, date = [], [], [], [], []
-
-### FOR LOOP through pages 1...448
-search_page = url.urlopen(search_url)
-page = search_page.read().decode("utf8")
-
-
-idx, doi_end = 0, 0
-
-while (idx + doi_end) < len(page):
+### for yr 2012 .. 2024
+yr = 2023
+### for volume 1 .. 10
+vl = 1
+### FOR LOOP through pages 1...20
+def clip(target, strt, fnsh, offset = 0):
+    STRT = re.search(strt, target) 
+    if STRT is None:
+        return STRT
+    FNSH = STRT.span()[1] + offset + re.search(fnsh, target[STRT.span()[1] + offset:]).span()[0]
+    return target[STRT.span()[1]+offset:FNSH]
     
-    idx = 0 + doi_end
-    doi_start = idx + re.search("\/articles\/s", page[idx:]).span()[1]
-    doi_end = doi_start + re.search("\"", page[doi_start:]).span()[0]
-    doi = page[doi_start:doi_end]
-    article_url = f"{base_url}/articles/s{doi}"
-    urls.append(article_url)
-    print(article_url)
-    ## parsed from query page
-    article_page = url.urlopen(article_url)
-    article_page = article_page.read().decode("utf8")
+for pg in range(3,21):
+
+
+    # def fetch_data_tbl(yr, vl, pg, base_url = "https://www.nature.com"):
+    search_url = f"{base_url}/search?date_range={yr}-{yr}&volume={vl}&page={pg}"
+    dois, urls, auth, email, title, date = [], [], [], [], [], []
+
+    search_page = url.urlopen(search_url)
+    page = search_page.read().decode("utf8")
+
+    #### Subjects tbl
+    dois_tbl, sub_tbl = [], np.array([])
+
+    idx, doi_end = 0, 0
     
-    ## authors "contentInfo":{"authors"
-    auth_span_start = re.search("\"authors\"", article_page).span()[1] + 3
-    auth_span_end = auth_span_start + re.search("\"", article_page[auth_span_start:]).span()[0]
-    auth.append(article_page[auth_span_start:auth_span_end])
-    ## correspond. auth. "email":"claus.lamm@univie.ac.at"
-    cauth_span_start = re.search("\"email\":", article_page).span()[1] + 3
-    cauth_span_end = cauth_span_start + re.search("\"", article_page[cauth_span_start:]).span()[0]
-    email.append(article_page[cauth_span_start:cauth_span_end])
-    ## <title>
-    title_span_start = re.search("<title>", article_page).span()[1]
-    title_span_end = title_span_start + re.search("</title>", article_page[title_span_start:]).span()[0] 
-    title.append(article_page[title_span_start:title_span_end])
-    ## "datePublished"
-    date_span_start = re.search("datePublished", article_page).span()[1] + 3
-    date_span_end = date_span_start + re.search("\"", article_page[date_span_start:]).span()[0] 
-    date.append(article_page[date_span_start:date_span_end])
-## SAVES results
-results = pd.DataFrame(dict([("author",auth), ("email",email),("url",urls),("date",date)]))
-results.to_csv("page1_test.csv", index = False)
+
+    while (idx + doi_end) < len(page):
+        
+        idx = 0 + doi_end
+        doi_start = idx + re.search("\/articles\/s", page[idx:]).span()[1]
+        doi_end = doi_start + re.search("\"", page[doi_start:]).span()[0]
+        doi = page[doi_start:doi_end]
+        article_url = f"{base_url}/articles/s{doi}"
+        dois.append(doi)
+        urls.append(article_url)
+        print(article_url)
+        ## parsed from query page
+        article_page = url.urlopen(article_url)
+        article_page = article_page.read().decode("utf8")
+        ## authors "contentInfo":{"authors"
+        art_aut = clip(article_page, "\"authors\"", "\"", offset=3)
+        auth.append(art_aut) 
+        ## correspond. auth. "email":"claus.lamm@univie.ac.at"
+        art_cauth = clip(article_page, "\"email\":", "\"", offset=3)
+        email.append(art_cauth) if art_cauth is not None else email.append("")
+        ## <title>
+        art_title = clip(article_page, "<title>", "</title>")
+        title.append(art_title)
+        ## "datePublished"
+        art_date = clip(article_page, "datePublished", "\"", offset = 3)
+        date.append(art_date)
+        ## subjects
+        art_subjects = clip(article_page, "subjects\":\"", "\"")
+        art_subjects = art_subjects.split(",")
+        sub_tbl = np.concatenate([sub_tbl, art_subjects])
+        [dois_tbl.append(doi) for i in range(len(art_subjects))]
+
+    ### SAVES the subjects table    
+    subjects_table = pd.DataFrame(dict([("doi", dois_tbl), ("subject",sub_tbl)]))
+    subjects_table.to_csv(f"subjects_{yr}_vol{vl}_{pg}.csv", index = False)
+
+    ## SAVES the urls
+    results = pd.DataFrame(dict([("doi", dois), ("author",auth), ("title", title), ("email",email),("url",urls),("date",date)]))
+    results.to_csv(f"urls_{yr}_vol{vl}_{pg}.csv", index = False)
+
 pdb.set_trace()
