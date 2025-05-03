c4662cbd24288aec9487580916f74beba9dd0c07
sauves <leonardsauve@gmail.com>
2025-05-03 01:39:28 -0400

    added Nature data

--- compute_scores.py
+++ compute_scores.py
@@ -4,12 +4,17 @@ import pdb
 import re
 import os 
 data_list = []
+subjects_list = []
 for yr in [2020,2021,2022,2023,2024,2025]:
     yr = 2020
     for fname in os.listdir(f"Articles_Data/articles_{yr}"):
         if "scores" in fname:
             data_list.append(pd.read_csv(f"Articles_Data/articles_{yr}/{fname}", sep = "\t"))
+        if "subjects" in fname:
+            subjects_list.append(pd.read_csv(f"Articles_Data/articles_{yr}/{fname}", sep = "\t"))
 pd.concat(data_list).to_csv("sample_data/Nature_scores.csv")
+pd.concat(subjects_list).to_csv("sample_data/Nature_subjects.csv")
+
 pdb.set_trace()
 
 yr = 2025
